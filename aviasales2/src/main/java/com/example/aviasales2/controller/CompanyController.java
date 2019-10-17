19b4fd9c1a13c5d9cb2d25e852ad41bfe29bcfa1
Danila <danila-pinaev@mail.ru>
2019-10-17 16:54:45 +0300

    Company full crud+

---
+++ aviasales2/src/main/java/com/example/aviasales2/controller/CompanyController.java
@@ -0,0 +1,40 @@
+package com.example.aviasales2.Controller;
+
+import com.example.aviasales2.Entity.Company;
+import com.example.aviasales2.Service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    @Autowired
+    CompanyService companyService;
+
+    @GetMapping("/getById")
+    private Company getCompanyById(@RequestParam("id")long id){return companyService.findByCompanyId(id);}
+
+    @PostMapping("/save")
+    private Company saveCompany(@RequestBody Company company){return companyService.save(company);}
+
+    @GetMapping("/delete")
+    private String deleteCompany(@RequestParam("id") long id){return companyService.delete(id);}
+
+    @GetMapping("/getByName")
+    private Company getCompanyByName(@RequestParam(name = "name")String name){return companyService.findByCompanyName(name);}
+
+    @GetMapping("/getByTransId")
+    private List<Company> getCompanyByTransId(@RequestParam("id")long id){return companyService.findAllByTransportId(id);}
+
+    @PostMapping("/update")
+    private Company updateCompany(@RequestBody Company updatedCompany){return companyService.save(updatedCompany);}
+
+    @GetMapping("/getByRating")
+    private List<Company> getCompanyByRating(@RequestParam("rating")int rating){return companyService.findByRating(rating);}
+
+
+}
---
+++ aviasales2/src/main/java/com/example/aviasales2/entity/Company.java
@@ -0,0 +1,67 @@
+package com.example.aviasales2.Entity;
+
+import lombok.AllArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@AllArgsConstructor
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue
+    long companyId;
+
+    String companyName;
+
+    long transportId;
+
+    long commentId;
+
+    int rating;
+
+    public Company(){}
+
+    public long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public long getTransportId() {
+        return transportId;
+    }
+
+    public void setTransportId(long transportId) {
+        this.transportId = transportId;
+    }
+
+    public long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(long commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
