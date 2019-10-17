e2dc1f00f3ced048565f618f22ebfc6a37188f05
Egor Kamysh <egorkamysh99@gmail.com>
2019-10-17 16:17:31 +0300

    Added tour

---
+++ aviasales2/src/main/java/com/example/aviasales2/repository/TourRepository.java
@@ -0,0 +1,14 @@
+package com.example.aviasales2.repository;
+
+        import com.example.aviasales2.entity.Tour;
+        import org.springframework.data.repository.CrudRepository;
+        import org.springframework.stereotype.Repository;
+
+        import java.util.List;
+@Repository
+public interface TourRepository extends CrudRepository<Tour, Long> {
+    Tour findByName(String name);
+    List<Tour> findAll();
+    Tour deleteById(long id);
+
+}
