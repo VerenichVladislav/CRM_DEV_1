9f090280187e04c4d834ae2691fcc0cac8e6fe7d
Eugenie Chupris <zhenuh15@gmail.com>
2019-10-18 08:35:22 +0300

    add CRUD Transport

--- aviasales2/src/main/java/com/example/aviasales2/Aviasales2Application.java
+++ aviasales2/src/main/java/com/example/aviasales2/Aviasales2Application.java
@@ -1,13 +1,19 @@
 package com.example.aviasales2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
 
 @SpringBootApplication
 public class Aviasales2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aviasales2Application.class, args);
+
+
 	}
 
 }
