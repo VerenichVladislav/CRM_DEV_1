b9296867b52cef70f114fd9bc5b91f05a44ec021
Eugenie Chupris <zhenuh15@gmail.com>
2019-10-28 12:02:17 +0300

    add HTTPS protocol

--- aviasales2/src/main/java/com/example/aviasales2/Aviasales2Application.java
+++ aviasales2/src/main/java/com/example/aviasales2/Aviasales2Application.java
@@ -21,42 +21,4 @@ public class Aviasales2Application {
 
 
 	}
-
-/*
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		// Enable SSL Trafic
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		// Add HTTP to HTTPS redirect
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-
-
-
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8082);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	}
-
-
-
- */
 }
