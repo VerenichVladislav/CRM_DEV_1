c575746669e857b575d7388a59db043ebf3b0661
Eugenie Chupris <zhenuh15@gmail.com>
2019-11-02 18:43:26 +0300

    add Sender

---
+++ aviasales2/src/main/java/com/example/aviasales2/entity/Sender.java
@@ -0,0 +1,41 @@
+package com.example.aviasales2.entity;
+
+import javax.mail.*;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Sender {
+    private String username = "cracktheworld@mail.ru";
+    private String password = "Nib-A7f-hq2-Nur";
+    private Properties properties;
+    public Sender (){
+        properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.mail.ru");
+        properties.put("mail.debug", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.user", username);
+        properties.put("mail.smtp.port", 465);
+        properties.put("mail.smtp.socketFactory.port", 465);
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.socketFactory.fallback", "false");
+    }
+    public  void send(String subject,String text,String toEmail){
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(username,password);
+            }
+        });
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+        } catch (MessagingException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
