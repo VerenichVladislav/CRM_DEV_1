3354ac911757dfcfae7d62021cbc636e7a0baae0
Egor Kamysh <egorkamysh99@gmail.com>
2019-11-15 00:21:19 +0300

    Wallet operations with hibernate's magic

--- aviasales2/src/main/java/com/example/aviasales2/repository/WalletRepository.java
+++ aviasales2/src/main/java/com/example/aviasales2/repository/WalletRepository.java
@@ -1,7 +1,9 @@
 package com.example.aviasales2.repository;
 
 import com.example.aviasales2.entity.Wallet;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,5 @@ public interface WalletRepository extends CrudRepository<Wallet, Long> {
 
     List<Wallet> findAll();
     Wallet findById(long id);
+    Wallet findByOwnerId(long id);
 }
