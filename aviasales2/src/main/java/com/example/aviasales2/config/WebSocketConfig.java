02e9f5f075d925cef96892421a87f67328da3bf0
Danila <danila-pinaev@mail.ru>
2019-12-14 16:37:55 +0300

    added chat components

--- aviasales2/src/main/java/com/example/aviasales2/config/WebSocketConfig.java
+++ aviasales2/src/main/java/com/example/aviasales2/config/WebSocketConfig.java
@@ -2,22 +2,26 @@ package com.example.aviasales2.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import static com.example.aviasales2.Constants.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker(SECURED_CHAT_HISTORY, SECURED_CHAT_SPECIFIC_USER);
+        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
+        config.setUserDestinationPrefix("/secured/user");
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint(SECURED_CHAT_ROOM).withSockJS();
+        registry.addEndpoint(SECURED_CHAT).setAllowedOrigins("*").withSockJS();
     }
 }
