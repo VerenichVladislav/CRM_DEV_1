d437627dbba884946e6514e61fbf0fc5915eb195
dasha27atr <dasha27atr@gmail.com>
2019-12-15 19:26:21 +0300

    Main filter fixed

--- aviasales2/src/main/java/com/example/aviasales2/service/impl/HotelServiceImpl.java
+++ aviasales2/src/main/java/com/example/aviasales2/service/impl/HotelServiceImpl.java
@@ -4,19 +4,26 @@ import com.example.aviasales2.config.filterConfig.HotelFilter;
 import com.example.aviasales2.entity.Hotel;
 import com.example.aviasales2.entity.QHotel;
 import com.example.aviasales2.entity.QReservation;
+import com.example.aviasales2.entity.Reservation;
 import com.example.aviasales2.repository.CompanyRepository;
 import com.example.aviasales2.repository.HotelRepository;
 import com.example.aviasales2.repository.TourRepository;
 import com.example.aviasales2.service.HotelService;
-import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,6 +32,8 @@ public class HotelServiceImpl implements HotelService {
     private final HotelRepository hotelRepository;
     private final CompanyRepository companyRepository;
     private final TourRepository tourRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public HotelServiceImpl(HotelRepository hotelRepository, CompanyRepository companyRepository, TourRepository tourRepository) {
@@ -62,13 +71,19 @@ public class HotelServiceImpl implements HotelService {
     public List <Hotel> findAll(HotelFilter hotelFilter) {
         final QHotel qHotel = QHotel.hotel;
         final QReservation qReservation = QReservation.reservation;
-
-        BooleanBuilder booleanBuilder = new BooleanBuilder(qHotel.isNotNull());
+        JPAQuery<Hotel> hotelQuery = new JPAQuery<>(entityManager);
+        List<Hotel> hotels;
+        hotelQuery.from(qHotel)
+                .leftJoin(qHotel.reservations, qReservation)
+                .on(qReservation.hotel.hotelId.eq(qHotel.hotelId))
+                .where(qHotel.isNotNull());
         if (hotelFilter.getCity() != null) {
-            booleanBuilder.and(qHotel.city.cityName.eq(hotelFilter.getCity()));
+            BooleanExpression hotelCityName = qHotel.city.cityName.eq(hotelFilter.getCity());
+            hotelQuery.where(hotelCityName);
         }
         if (hotelFilter.getRating() != null) {
-            booleanBuilder.and(qHotel.rating.eq(hotelFilter.getRating()));
+            BooleanExpression hotelRating = qHotel.rating.eq(hotelFilter.getRating());
+            hotelQuery.where(hotelRating);
         }
         if (hotelFilter.getDateFrom() != null) {
             Timestamp firstTimestamp;
@@ -78,10 +93,21 @@ public class HotelServiceImpl implements HotelService {
             LocalDateTime localDateTime = localTime.atDate(localDate);
             firstTimestamp = Timestamp.valueOf(localDateTime.plusHours(3));
             secondTimestamp = Timestamp.valueOf(localDateTime.plusHours(26).plusMinutes(59).plusSeconds(59).plusNanos(999999999));
+            JPAQuery<Reservation> reservationQuery = new JPAQuery<>(entityManager);
+            List<Reservation> reservations = reservationQuery.from(qReservation).where(qReservation.checkIn.between(firstTimestamp, secondTimestamp)).fetch();
+            BooleanExpression hotelCheckIn = qReservation.checkIn.notBetween(firstTimestamp, secondTimestamp)
+                    .or(qHotel.reservations.isEmpty());
+            BooleanExpression hotelRoomsSize = qHotel.rooms.size().gt(reservations.size());
+            hotelQuery.where(hotelCheckIn.and(hotelRoomsSize));
 
-            booleanBuilder.and(qReservation.checkIn.between(firstTimestamp, secondTimestamp));
         }
-        return (List <Hotel>) hotelRepository.findAll(booleanBuilder);
+        hotels = hotelQuery
+                .fetch();
+        Set<Hotel> result = new HashSet<>(hotels);
+        hotels = new ArrayList<>(result);
+        hotels.sort((hotelsFirst, hotelsSorted) ->
+                (int) (hotelsFirst.getHotelId() - hotelsSorted.getHotelId()));
+        return hotels;
     }
 
     @Override
