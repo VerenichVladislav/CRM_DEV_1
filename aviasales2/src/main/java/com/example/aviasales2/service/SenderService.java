d541f91bfc7955b037c7bffee5ed86c26921b3b9
Egor Kamysh <egorkamysh99@gmail.com>
2019-12-26 12:57:08 +0300

    update sending email

--- aviasales2/src/main/java/com/example/aviasales2/service/SenderService.java
+++ aviasales2/src/main/java/com/example/aviasales2/service/SenderService.java
@@ -1,6 +1,6 @@
 package com.example.aviasales2.service;
 
 public interface SenderService {
-    void buyEmail(long userId, long tripId, String list, int count);
-    void buyEmailTour(long userId);
+    void buyEmail(Long userId, Long tripId, String list, int count);
+    void buyEmailTour(Long userId);
 }
--- aviasales2/src/main/java/com/example/aviasales2/service/impl/SenderServiceImpl.java
+++ aviasales2/src/main/java/com/example/aviasales2/service/impl/SenderServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.aviasales2.repository.TripRepository;
 import com.example.aviasales2.repository.UserRepository;
 import com.example.aviasales2.service.SenderService;
 import com.example.aviasales2.service.TripService;
+import com.example.aviasales2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,28 +16,30 @@ import java.text.SimpleDateFormat;
 
 @Service
 public class SenderServiceImpl implements SenderService {
-    private final TripRepository tripRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final TripService tripService;
 
     @Autowired
-    public SenderServiceImpl(TripRepository tripRepository, UserRepository userRepository, TripService tripService) {
-        this.tripRepository = tripRepository;
-        this.userRepository = userRepository;
+    public SenderServiceImpl(UserService userService, TripService tripService) {
+        this.userService = userService;
         this.tripService = tripService;
     }
 
     @Override
-    public void buyEmail(long userId, long tripId, String list, int count) {
-        Trip trip = tripRepository.findByTripId(tripId);
-        User buyer = userRepository.findByUserId(userId);
-        City cityFrom = trip.getCityFrom();
-        City cityDest = trip.getCityDest();
+    public void buyEmail(Long userId, Long tripId, String list, int count) {
+        Trip trip = tripService.findById(tripId);
+        User buyer = userService.findById(userId);
+        String cityFrom = trip.getCityFrom().getCityName();
+        String cityDest = trip.getCityDest().getCityName();
         String date = new SimpleDateFormat("HH:mm MM/dd/yyyy").format(trip.getDateFrom());
+        sendEmail(tripId, list, count, buyer, cityFrom, cityDest, date);
+    }
+
+    private void sendEmail(Long tripId, String list, int count, User buyer, String cityFrom, String cityDest, String date) {
         StringBuffer sb = new StringBuffer();
         sb.append("Hello, " + buyer.getUserName() + "!\n\n");
-        sb.append("You bought " + count + " ticket(s) from " + cityFrom.getCityName() +
-                " to " + cityDest.getCityName() + "!\n\n");
+        sb.append("You bought " + count + " ticket(s) from " + cityFrom +
+                " to " + cityDest + "!\n\n");
         sb.append("Date: " + date + "\n");
         sb.append("Total Price: " + tripService.calculateCost(count, tripId) + "$\n\n");
         sb.append("Passengers: \n");
@@ -47,10 +50,11 @@ public class SenderServiceImpl implements SenderService {
         Sender sender = new Sender();
         sender.send("Purchase information from AviaSales2.0", text, eml);
     }
-    @Override
-    public void buyEmailTour(long userId) {
 
-        User buyer = userRepository.findByUserId(userId);
+    @Override
+    public void buyEmailTour(Long userId) {
+
+        User buyer = userService.findById(userId);
 
         StringBuffer sb = new StringBuffer();
         sb.append("Hello, " + buyer.getUserName() + "!\n\n");
